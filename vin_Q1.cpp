475c823721a9f38acab8aab0fa16a195b6b8003f
Vinit Savani <136707852+VinitSavani@users.noreply.github.com>
2023-08-19 22:20:05 +0530

    Add files via upload

---
+++ vin_Q1.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+class Train 
+{
+public:
+    int trainNumber;
+    string trainName;
+    string source;
+    string destination;
+    string trainTime;
+
+    void inputTrainDetails() 
+	{
+        cout << endl << "Enter Train Number: ";
+        cin >> trainNumber;
+        cout << "Enter Train Name: ";
+        cin >> trainName;
+        cout << "Enter Source: ";
+        cin >> source;
+        cout << "Enter Destination: ";
+        cin >> destination;
+        cout << "Enter Train Time: ";
+        cin >> trainTime;
+        cout << endl ;
+    }
+
+    void displayTrainDetails() 
+	{
+        cout << endl << "Train Number: " << trainNumber << endl;
+        cout << "Train Name: " << trainName << endl;
+        cout << "Source: " << source << endl;
+        cout << "Destination: " << destination << endl;
+        cout << "Train Time: " << trainTime << endl;
+        cout << "-----------------------------------------" << endl ;
+    }
+
+    int searchTrain(int searchNumber) {
+        return (trainNumber == searchNumber) ? 1 : 0;
+    }
+};
+
+int main() 
+{
+    int N;
+    
+    cout << endl << "/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/" << endl;
+    cout << "Train Record input and serch program " << endl ;
+    cout << "/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/" << endl;
+    
+    cout << endl << "Enter the number of train records: ";
+    cin >> N;
+
+    if (N < 3) {
+        cout << "Minimum 3 train records are required." << endl;
+        return 1;
+    }
+
+    Train trains[N];
+
+    for (int i = 0; i < N; ++i) {
+        cout << "Enter details for Train " << i + 1 << ":" << endl;
+        trains[i].inputTrainDetails();
+    }
+
+    int searchNumber;
+    cout << "Enter Train Number to search: ";
+    cin >> searchNumber;
+
+    int foundIndex = -1;
+    for (int i = 0; i < N; ++i) {
+        if (trains[i].searchTrain(searchNumber)) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    if (foundIndex != -1) {
+        cout << "Train details for Train Number " << searchNumber << ":" << endl;
+        trains[foundIndex].displayTrainDetails();
+    } else {
+        cout << "Train with Train Number " << searchNumber << " not found." << endl;
+    }
+
+    return 0;
+}
