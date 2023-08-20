ed3c153a1da9e9fbddfe310c86021cb629c01a4c
Vinit Savani <136707852+VinitSavani@users.noreply.github.com>
2023-08-20 11:19:30 +0530

    Update vin_Q3.cpp

--- vin_Q3.cpp
+++ vin_Q3.cpp
@@ -2,7 +2,8 @@
 #include <cstdlib>
 using namespace std;
 
-class Person {
+class Person 
+{
 public:
     string name;
     long accountNumber;
@@ -10,12 +11,14 @@ public:
     string email;
 };
 
-class Bank {
+class Bank 
+{
 private:
     Person loggedInUser;
     double accountBalance = 200000;
 public:
-    void clearScreen() {
+    void clearScreen() 
+	{
         #ifdef _WIN32
         system("cls");
         #else
@@ -23,174 +26,207 @@ public:
         #endif
     }
 
-    void displayIntroScreen() {
+    void displayIntroScreen() 
+	{
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*        Welcome to the Bank System          *" << endl;
-        cout << "*           Press any key to continue        *" << endl;
-        cout << "**********************************************" << endl;
+        cout << endl << endl << endl ;
+        cout << "                         -: BANK MANAGEMENT SYSTEM :-               " << endl;
+        cout << "          ----------------------------------------------------------" << endl << endl ;
+        cout << "                         -: Design & Programed By :-               " << endl;
+        cout << "          ----------------------------------------------------------" << endl ;
+        cout << "                               VINIT S SAVANI                     " << endl << endl << endl ;
+        cout << "          ----------------------------------------------------------" << endl;
+        cout << "                     Press Any key N Enter To Continue.........." << endl;
         cin.get();
-        cin.ignore();
     }
 
-    char displayLoginScreen() {
+    char displayLoginScreen() 
+	{
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*         Login as Admin or Staff            *" << endl;
-        cout << "**********************************************" << endl;
-        cout << "*    A. Login as Admin                       *" << endl;
-        cout << "*    S. Login as Staff                       *" << endl;
-        cout << "**********************************************" << endl;
+        cout << endl << endl << endl ;
+        cout << "          **********************************************" << endl;
+        cout << "          *               Welcome TO SBI               *" << endl;
+        cout << "          **********************************************" << endl << endl << endl;
+        cout << "          **********************************************" << endl;
+        cout << "          *        Press A To Login as Admin           *" << endl;
+        cout << "          *        Press S To Login as Staff           *" << endl;
+        cout << "          **********************************************" << endl;
+        
         char choice;
+        
         cin >> choice;
         return choice;
     }
 
-    void displayAdminLoginScreen() {
+    void displayAdminLoginScreen() 
+	{
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*         Admin Login                        *" << endl;
-        cout << "**********************************************" << endl;
+        cout << "          **********************************************" << endl;
+        cout << "          *               Admin Login                  *" << endl;
+        cout << "          **********************************************" << endl;
     }
 
-    void displayStaffLoginScreen() {
+    void displayStaffLoginScreen() 
+	{
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*         Staff Login                        *" << endl;
-        cout << "**********************************************" << endl;
+        cout << "          **********************************************" << endl;
+        cout << "          *              Staff Login                   *" << endl;
+        cout << "          **********************************************" << endl;
     }
 
-    void loginAsStaff() {
+    void loginAsStaff() 
+	{
         displayStaffLoginScreen();
-        cout << "Enter name: ";
+        cout << "          Enter name: ";
         cin >> loggedInUser.name;
-        cout << "Enter account number: ";
+        cout << "          Enter account number: ";
         cin >> loggedInUser.accountNumber;
-        cout << "Enter phone number: ";
+        cout << "          Enter phone number: ";
         cin >> loggedInUser.phoneNumber;
-        cout << "Enter email: ";
+        cout << "          Enter email: ";
         cin >> loggedInUser.email;
     }
 
-    void loginAsAdmin() {
+    void loginAsAdmin() 
+	{
         displayAdminLoginScreen();
-        cout << "Enter name: ";
+        cout << "          Enter name: ";
         cin >> loggedInUser.name;
-        cout << "Enter account number: ";
+        cout << "          Enter account number: ";
         cin >> loggedInUser.accountNumber;
-        cout << "Enter phone number: ";
+        cout << "          Enter phone number: ";
         cin >> loggedInUser.phoneNumber;
-        cout << "Enter email: ";
+        cout << "          Enter email: ";
         cin >> loggedInUser.email;
     }
 
-    void displayUserInfo() {
-        cout << "Name: " << loggedInUser.name << endl;
-        cout << "Account Number: " << loggedInUser.accountNumber << endl;
-        cout << "Phone Number: " << loggedInUser.phoneNumber << endl;
-        cout << "Email: " << loggedInUser.email << endl;
+    void displayUserInfo() 
+	{
+        cout << endl << "          Name: " << this->loggedInUser.name << endl;
+        cout << "          Account Number: " << this->loggedInUser.accountNumber << endl;
+        cout << "          Phone Number: " << this->loggedInUser.phoneNumber << endl;
+        cout << "          Email: " << this->loggedInUser.email << endl << endl << endl ;
     }
 
-    void displayOptions() {
-        clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*                 Options                    *" << endl;
-        cout << "**********************************************" << endl;
-        cout << "*    1. Deposit Money                        *" << endl;
-        cout << "*    2. Transfer Money                       *" << endl;
-        cout << "*    3. Withdraw Money                       *" << endl;
-        cout << "*    0. Exit                                 *" << endl;
-        cout << "**********************************************" << endl;
-        cout << "Current Balance: " << accountBalance << endl;
+    void displayOptions() 
+	{
+        cout << "          **********************************************" << endl;
+        cout << "          *                 Options                    *" << endl;
+        cout << "          **********************************************" << endl;
+        cout << "          *    1. Deposit Money                        *" << endl;
+        cout << "          *    2. Transfer Money                       *" << endl;
+        cout << "          *    3. Withdraw Money                       *" << endl;
+        cout << "          *    0. Exit                                 *" << endl;
+        cout << "          **********************************************" << endl << endl ;
+        cout << "               Current Balance: " << accountBalance << endl;
     }
 
-    void performDeposit() {
+    void performDeposit() 
+	{
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*            Deposit Money                   *" << endl;
-        cout << "**********************************************" << endl;
-        cout << "Current Balance: " << accountBalance << endl;
-        cout << "Enter the amount to deposit: ";
+        cout << endl << endl << endl ;
+        cout << "          **********************************************" << endl;
+        cout << "          *            Deposit Money                   *" << endl;
+        cout << "          **********************************************" << endl << endl ;
+        cout << "          Current Balance: " << accountBalance << endl;
+        cout << endl << "          Enter the amount to deposit: ";
         double amount;
         cin >> amount;
-        if (amount <= 0) {
-            cout << "Invalid amount." << endl;
-        } else {
+        if (amount <= 0) 
+		{
+            cout << "          Invalid amount." << endl;
+        } else 
+		{
             accountBalance += amount;
-            cout << "Deposit successful. Current balance: " << accountBalance << endl;
+            cout << endl << "          Deposit successful.  " << endl << endl ;
+            cout << "          Current balance: " << accountBalance << endl << endl;
         }
     }
 
     void performTransfer() {
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*           Transfer Money                   *" << endl;
-        cout << "**********************************************" << endl;
-        cout << "Current Balance: " << accountBalance << endl;
-        cout << "Enter the amount to transfer: ";
+        cout << endl << endl << endl ;
+        cout << "          **********************************************" << endl;
+        cout << "          *           Transfer Money                   *" << endl;
+        cout << "          **********************************************" << endl << endl ;
+        cout << "          Current Balance: " << accountBalance << endl;
+        cout << "          Enter the amount to transfer: ";
         double amount;
         cin >> amount;
-        if (amount <= 0) {
-            cout << "Invalid amount." << endl;
-        } else if (amount > accountBalance) {
-            cout << "Insufficient funds." << endl;
-        } else {
+        if (amount <= 0)
+		{
+            cout << "          Invalid amount." << endl;
+        } else if (amount > accountBalance) 
+		{
+            cout << "          Insufficient funds." << endl;
+        } else 
+		{
             accountBalance -= amount;
-            cout << "Transfer successful. Current balance: " << accountBalance << endl;
+            cout << endl << "          Transfer successful." << endl << endl;
+            cout << "          Current balance: " << accountBalance << endl << endl;
         }
     }
 
-    void performWithdraw() {
+    void performWithdraw() 
+	{
         clearScreen();
-        cout << "**********************************************" << endl;
-        cout << "*           Withdraw Money                   *" << endl;
-        cout << "**********************************************" << endl;
-        cout << "Current Balance: " << accountBalance << endl;
-        cout << "Enter the amount to withdraw: ";
+        cout << endl << endl << endl ;
+        cout << "          **********************************************" << endl;
+        cout << "          *           Withdraw Money                   *" << endl;
+        cout << "          **********************************************" << endl << endl ;
+        cout << "          Current Balance: " << accountBalance << endl;
+        cout << "          Enter the amount to withdraw: ";
         double amount;
         cin >> amount;
-        if (amount <= 0) {
-            cout << "Invalid amount." << endl;
-        } else if (amount > accountBalance) {
-            cout << "Insufficient funds." << endl;
-        } else {
+        if (amount <= 0) 
+		{
+            cout << "          Invalid amount." << endl;
+        } else if (amount > accountBalance) 
+		{
+            cout << "          Insufficient funds." << endl;
+        } else 
+		{
             accountBalance -= amount;
-            cout << "Withdrawal successful. Current balance: " << accountBalance << endl;
+            cout << endl << "          Withdrawal successful."<< endl << endl;
+            cout << "          Current balance: " << accountBalance << endl << endl;
         }
     }
 };
 
-int main() {
+int main() 
+{
     Bank bank;
 
     bank.displayIntroScreen();
     
     char loginChoice;
-    while (true) {
+    
+    while (true) 
+	{
         bank.clearScreen();
         loginChoice = bank.displayLoginScreen();
-        if (loginChoice == 'S' || loginChoice == 's') {
+        if (loginChoice == 'S' || loginChoice == 's') 
+		{
             bank.displayStaffLoginScreen();
             bank.loginAsStaff();
             break;
-        } else if (loginChoice == 'A' || loginChoice == 'a') {
+        } else if (loginChoice == 'A' || loginChoice == 'a') 
+		{
             bank.displayAdminLoginScreen();
             bank.loginAsAdmin();
             break;
         }
     }
 
-    bank.clearScreen();
-    
-    cout << "User Information:" << endl;
-    bank.displayUserInfo();
-
-    while (true) {
+    while (true) 
+	{
         bank.clearScreen();
+        cout << endl << endl << "          User Information:" << endl;
+        bank.displayUserInfo();
         bank.displayOptions();
         
         int option;
-        cout << "Select an option: ";
+        cout << endl << endl << "                Select an option: ";
         cin >> option;
 
         switch (option) {
@@ -204,13 +240,13 @@ int main() {
                 bank.performWithdraw();
                 break;
             case 0:
-                cout << "Exiting the Bank System." << endl;
+                cout << "          Exiting the Bank System." << endl;
                 return 0;
             default:
-                cout << "Invalid option." << endl;
+                cout << "          Invalid option." << endl;
         }
 
-        cout << "Press any key to continue...";
+        cout << "          Press any key to continue......";
         cin.get();
         cin.ignore();
     }
